22f2183d159b92a7f53dc95a8fe631425b7a841e
Haochen Xie <haochen@xie.name>
2015-05-13 15:31:06 +0900

    src/main/docker - add first docker build file
    the current Dockerfile builds a container with emscripten
    installed and warmed up.
    
    Signed-off-by: Haochen Xie <haochen@xie.name>

---
+++ src/main/docker/README.md
@@ -0,0 +1,2 @@
+src/main/docker/ - currently contains build script for building a
+docker image with emscripten installed and warmed up.
---
+++ src/main/docker/sample.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void) {
+  int x = 132, y = 395;
+
+  printf ("hello world!\n");
+  printf ("we have two integers: %d %d\n", x, y);
+
+  printf ("they add up as %d\n", x + y);
+
+  return 0;
+}
